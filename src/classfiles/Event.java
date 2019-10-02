3fdfb5393988bd8e52dc5d6b436dd9ccdae763df
m0canu1 <alexandrumocanu9@gmail.com>
2019-10-02 15:13:52 +0200

    Aggiunta Lista di Task in Event

--- src/classfiles/Event.java
+++ src/classfiles/Event.java
@@ -1,11 +1,14 @@
 package classfiles;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Event {
     
     private int id;
     private String name;
     private Menu menu_selected;
-    private Summary summarySheet;
+    private List<Task> taskList = new ArrayList<>();
 
     public Event(String name) {this.name = name;}
 
@@ -13,19 +16,17 @@ public class Event {
 
     public Menu getMenu() {return this.menu_selected;}
 
-    public Summary getSummarySheet() {return this.summarySheet;}
-
     public void setMenu(Menu m) {this.menu_selected = m;}
-    public void setSummarySheet(Summary s) {this.summarySheet = s;}
 
-    public boolean hasSummarySheet() {return this.summarySheet != null;}
+    public void addTask(Task task) {this.taskList.add(task);}
 
-    public void addTask(Recipe r) {summarySheet.addTask(r);}
-
-    public boolean hasTask(Recipe r) {
-        return summarySheet.hasTask(r);
+    public boolean hasTask(Task task) {
+        return this.taskList.contains(task);
     }
 
-    public void deleteTask(Recipe r) {summarySheet.deleteTask(r);}
+    public void deleteTask(Task task) {this.taskList.remove(task);}
 
+    public List<Task> getTaskList() {
+        return taskList;
+    }
 }
